8ff24dc3df80c163b84316a90253283f8f6e6c36
webstar <webstar1987923@hotmail.com>
2018-01-10 02:51:55 +0300

    fixed mobile menu, drop menu, tiles

--- app/components/gd-header.js
+++ app/components/gd-header.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+	didInsertElement: function() {
+		Ember.run.scheduleOnce('afterRender', this, function() {
+            $(".menu-btn").click(function() {
+        		$(".menu-btn").toggleClass("active");
+        		$(".navbar").slideToggle(400);    	    	
+			});
+
+			$(window).resize(function(){
+  				if($(window).width() > 768){
+    				$('.navbar').removeAttr('style');
+  				}
+			});        		
+        });		
+	}
 });
